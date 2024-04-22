12976a23965fc0d9bed8500ff1a68f56fd767576
Wintec-Yuda <alamasta004@gmail.com>
2024-04-22 07:51:06 +0700

    resolve es lint

--- src/components/fragments/form/Athlete.tsx
+++ src/components/fragments/form/Athlete.tsx
@@ -9,7 +9,7 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Loader2 } from "lucide-react";
-import { Input } from "@/components/ui/input";
+import { Input } from "../../ui/input";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const formSchema = z.object({
